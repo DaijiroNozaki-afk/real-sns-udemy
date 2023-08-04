bbaed5a7b61da87c14182cbee078e2048e420e3e
dnozaki <dnozaki88888888@gmail.com>
2023-08-04 15:43:00 +0900

    make UploadApi u125

--- routes/upload.js
+++ routes/upload.js
@@ -1,4 +1,14 @@
 const router = require('express').Router();
 const multer = require('multer'); // この方法はローカルサーバーに画像を保存する。MongoDB に保存する方法もある
 
+const upload = multer();
+//画像アップロード用API
+router.post('/', upload.single('file'), (req, res) => {
+  try {
+    return res.status(200).json('画像アップロードに成功しました！');
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
